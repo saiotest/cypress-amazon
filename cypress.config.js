1b37e564501f7da4883566a64c05f8c563a7381c
Ely <elyermad@gmail.com>
2023-01-21 22:56:07 -0300

    mocha report

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+	projectId: "3wsh9o",
 	viewportWidth: 1920,
 	viewportHeight: 1080,
 	retries: 0,
@@ -8,6 +9,13 @@ module.exports = defineConfig({
 	video: true,
 	chromeWebSecurity: false,
 	screenshotOnRunFailure: true,
+	reporter: 'mochawesome',
+	reporterOptions: {
+	  reportDir: 'cypress/mochawesome-report',
+	  overwrite: true,
+	  html: true,
+	  json: true,
+	},
 	e2e: {
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
@@ -18,3 +26,4 @@ module.exports = defineConfig({
 
 	}
 });
+ 
